260f22fb6e3b639f26fc8636c07f3291fb8c02a6
laouissi <47759712+laouissi@users.noreply.github.com>
2021-01-06 22:55:04 +0100

    Add files via upload

---
+++ book_store/template/footer.php
@@ -0,0 +1,60 @@
+</main>
+<!-- !start #main-site -->
+
+<!-- start #footer -->
+<footer color=black ; id="footer" class="bg-dark text-white py-5">
+
+    <div class="container" >
+        <div class="row" style="padding: 50px 0px 10px 0px;">
+            <div class="col-lg-3 col-12">
+                <h4 class="font-rubik font-size-20">AGORA</h4>
+                <a href="http://localhost/book_store/index.php"class="font-size-14 font-rale text-white-50">heavan of readers.</a>
+            </div>
+            <div class="col-lg-4 col-12">
+                <h4 class="font-rubik font-size-20">Newslatter</h4>
+                <form class="form-row">
+                    <div class="col">
+                        <input type="text" class="form-control" placeholder="Email *">
+                    </div>
+                    <div class="col">
+                        <button type="submit" class="btn btn-primary mb-2">Subscribe</button>
+                    </div>
+                </form>
+            </div>
+            <div class="col-lg-2 col-12">
+                <h4 class="font-rubik font-size-20">Information</h4>
+                <div class="d-flex flex-column flex-wrap">
+                <a href="#" class="font-rale font-size-14 text-white-50 pb-1">About Us</a><br>
+                    <a href="http://localhost/book_store/category_list.php" class="font-rale font-size-14 text-white-50 pb-1">categore</a><br>
+                    <a href="http://localhost/book_store/books.php" class="font-rale font-size-14 text-white-50 pb-1">books</a>
+                </div>
+            </div>
+            <div class="col-lg-2 col-12">
+                <h4 a href="#" class="font-rubik font-size-20">Account</h4>
+                <div class="d-flex flex-column flex-wrap">
+                    <a href="#" class="font-rale font-size-14 text-white-50 pb-1">MY acount</a>
+               
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<div class="copyright text-center bg-dark text-white py-2 " style="margin: 0px 0px 100px 0px;">
+    <p class="font-rale font-size-14">&copy; Copyrights 2020. Desing By <a href="#" class="color-second">AGORA</a></p>
+</div>
+<!-- !start #footer -->
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.0/jquery.min.js" integrity="sha256-xNzN2a4ltkB44Mc/Jz3pT4iU1cmeR0FkXs4pru/JxaQ=" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+<!-- Owl Carousel Js file -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js" integrity="sha256-pTxD+DSzIwmwhOqTFN+DB+nHjO4iAsbgfyFq5K5bcE0=" crossorigin="anonymous"></script>
+
+<!--  isotope plugin cdn  -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.isotope/3.0.6/isotope.pkgd.min.js" integrity="sha256-CBrpuqrMhXwcLLUd5tvQ4euBHCdh7wGlDfNz8vbu/iI=" crossorigin="anonymous"></script>
+
+<!-- Custom Javascript -->
+<script src="index.js"></script>
+</body>
+</html>
